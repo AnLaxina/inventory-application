3db753d7cdabc1807a00233678163f098d0b935b
AnLaxina <anilovlaxina@gmail.com>
2026-01-06 09:49:41 -0800

    feat: reorganize file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// TODO: remove all of this and focus on fixing up the React structure
+
+// /client
+// ├── /src
+// │   ├── /api         <-- Functions to fetch data (fetchItems, deleteItem)
+// │   ├── /assets      <-- Images, global CSS
+// │   ├── /components  <-- Reusable UI (Button, NavBar, ItemCard)
+// │   ├── /pages       <-- Full views (Dashboard, ItemDetails, CreateForm)
+// │   ├── /hooks       <-- Custom hooks (optional, e.g., useInventory)
+// │   ├── App.jsx      <-- Router setup (React Router DOM)
+// │   └── main.jsx     <-- Entry point
+// └── vite.config.js
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h1>Hello, World!</h1>
+        </>
+    );
 }
 
-export default App
+export default App;
