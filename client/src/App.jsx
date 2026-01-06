084900b51009f35d76dbb7a744cf9df7dca5ccae
AnLaxina <anilovlaxina@gmail.com>
2026-01-06 09:55:34 -0800

    docs(app): remove comments for file structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,3 @@
-// TODO: remove all of this and focus on fixing up the React structure
-
-// /client
-// ├── /src
-// │   ├── /api         <-- Functions to fetch data (fetchItems, deleteItem)
-// │   ├── /assets      <-- Images, global CSS
-// │   ├── /components  <-- Reusable UI (Button, NavBar, ItemCard)
-// │   ├── /pages       <-- Full views (Dashboard, ItemDetails, CreateForm)
-// │   ├── /hooks       <-- Custom hooks (optional, e.g., useInventory)
-// │   ├── App.jsx      <-- Router setup (React Router DOM)
-// │   └── main.jsx     <-- Entry point
-// └── vite.config.js
-
 function App() {
     return (
         <>
