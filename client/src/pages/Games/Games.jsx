4a07a6131be4df3b056020602ec4cd32d9c9be5e
AnLaxina <anilovlaxina@gmail.com>
2026-01-08 13:53:34 -0800

    feat(games): add basic template for the `Games` page

---
+++ client/src/pages/Games/Games.jsx
@@ -0,0 +1,10 @@
+import Header from "../../components/Header/Header.jsx";
+import styles from "./games.module.css";
+
+export default function Games() {
+    return (
+        <section className={styles.games}>
+            <p>Welcome to the Games Section!</p>
+        </section>
+    );
+}
